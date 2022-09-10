9541dac4cb9b43e3d098bda95cd00827571cfe54
chadhaaa <chadha.hadji@gmail.com>
2022-09-10 16:25:23 +0100

    Added GeoLoc

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-
+import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  lat;
+  lng;
 
-  constructor() {}
-
+  constructor(private geo: Geolocation) {}
+  myLocation() {
+    this.geo.getCurrentPosition({timeout: 10000, enableHighAccuracy: true}).then((res) => {
+      this.lat = res.coords.latitude;
+      this.lng = res.coords.longitude;
+    }).catch((e) => {
+      console.log(e);
+    });
+  }
 }
